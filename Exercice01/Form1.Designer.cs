1456e14f86e0c2edd4306360c38f4befd6868990
Gaetan <poncin.gaetan1@gmail.com>
2016-07-28 11:06:16 +0200

    Integration des controle de validation dans le bouton valider

--- Exercice01/Form1.Designer.cs
+++ Exercice01/Form1.Designer.cs
@@ -28,12 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnValider = new System.Windows.Forms.Button();
+            this.btnEffacer = new System.Windows.Forms.Button();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.txtMontant = new System.Windows.Forms.TextBox();
+            this.txtCP = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nom";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Montant";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Code Postal";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(201, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "(JJ/MM/AAAA)";
+            // 
+            // btnValider
+            // 
+            this.btnValider.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnValider.Location = new System.Drawing.Point(201, 147);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(75, 23);
+            this.btnValider.TabIndex = 2;
+            this.btnValider.Text = "Valider";
+            this.btnValider.UseVisualStyleBackColor = true;
+            this.btnValider.Click += new System.EventHandler(this.btnValider_Click);
+            // 
+            // btnEffacer
+            // 
+            this.btnEffacer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEffacer.Location = new System.Drawing.Point(201, 176);
+            this.btnEffacer.Name = "btnEffacer";
+            this.btnEffacer.Size = new System.Drawing.Size(75, 23);
+            this.btnEffacer.TabIndex = 2;
+            this.btnEffacer.Text = "Effacer";
+            this.btnEffacer.UseVisualStyleBackColor = true;
+            this.btnEffacer.Click += new System.EventHandler(this.btnEffacer_Click);
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(81, 39);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(137, 20);
+            this.txtNom.TabIndex = 3;
+            // 
+            // txtDate
+            // 
+            this.txtDate.Location = new System.Drawing.Point(82, 68);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(113, 20);
+            this.txtDate.TabIndex = 3;
+            // 
+            // txtMontant
+            // 
+            this.txtMontant.Location = new System.Drawing.Point(81, 102);
+            this.txtMontant.Name = "txtMontant";
+            this.txtMontant.Size = new System.Drawing.Size(114, 20);
+            this.txtMontant.TabIndex = 3;
+            // 
+            // txtCP
+            // 
+            this.txtCP.Location = new System.Drawing.Point(81, 133);
+            this.txtCP.Name = "txtCP";
+            this.txtCP.Size = new System.Drawing.Size(83, 20);
+            this.txtCP.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 211);
+            this.Controls.Add(this.txtCP);
+            this.Controls.Add(this.txtMontant);
+            this.Controls.Add(this.txtDate);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.btnEffacer);
+            this.Controls.Add(this.btnValider);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnValider;
+        private System.Windows.Forms.Button btnEffacer;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.TextBox txtDate;
+        private System.Windows.Forms.TextBox txtMontant;
+        private System.Windows.Forms.TextBox txtCP;
     }
 }
 
--- Exercice01/Form1.cs
+++ Exercice01/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace Exercice01
@@ -15,6 +16,132 @@ namespace Exercice01
         public Form1()
         {
             InitializeComponent();
+        }
+
+        bool CheckNumber(string nombre)
+        {
+            string check = "0123456789.";
+            int controle = 0;
+
+            for (int i = 0; i < nombre.Length; i++)
+            {
+                if (check.IndexOf(nombre.Substring(i, 1)) == -1)
+                {
+                    controle++;
+                }
+            }
+
+            if (controle == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        bool CheckLetter(string nom)
+        {
+            string check = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int controle = 0;
+
+            for (int i = 0; i < nom.Length; i++)
+            {
+                if (check.IndexOf(nom.Substring(i, 1)) == -1)
+                {
+                    controle++;
+                }
+            }
+
+            if (controle == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+
+        private void btnValider_Click(object sender, EventArgs e)
+        {
+            bool test = true;
+            //Test du txtNom
+            if (CheckLetter(txtNom.Text) && txtNom.Text.Length !=0)
+            {
+                
+                txtNom.BackColor = Color.White;
+            }
+            else
+            {
+                test = false;
+                txtNom.BackColor = Color.Red;
+            }
+            //Test de la date
+            if (Regex.IsMatch(txtDate.Text, @"^[0-3][0-9]\/[0-1][0-9]\/[0-9][0-9]([0-9][0-9])?$") && txtDate.Text.Length == 10)
+            {
+                
+                txtDate.BackColor = Color.White;
+            }
+            else
+            {
+                test = false;
+                txtDate.BackColor = Color.Red;
+            }
+            //Test du montant
+            if (Regex.IsMatch(txtMontant.Text, @"^[0-9]+\.[0-9][0-9]?$"))
+            {                
+                txtMontant.BackColor = Color.White;
+            }
+            else
+            {
+                test = false;
+                txtMontant.BackColor = Color.Red;
+            }
+            //Test du code postal
+            if (Regex.IsMatch(txtCP.Text, @"^[0-9][0-9][0-9][0-9][0-9]?$"))
+            {            
+                txtCP.BackColor = Color.White;
+            }
+            else
+            {
+                test = false;
+                txtCP.BackColor = Color.Red;
+            }
+
+            if (test)
+            {
+                txtMontant.BackColor = Color.White;
+                txtCP.BackColor = Color.White;
+                txtDate.BackColor = Color.White;
+                txtNom.BackColor = Color.White;
+                MessageBox.Show("Nom : "+txtNom.Text + "\nDate : "+txtDate.Text + "\nMontant : "+txtMontant.Text+"\nCode : "+txtCP.Text, "Validation effectuée", MessageBoxButtons.OK);
+            }
+
+        }
+
+        private void btnEffacer_Click(object sender, EventArgs e)
+        {
+            txtCP.Clear();
+            txtDate.Clear();
+            txtMontant.Clear();
+            txtNom.Clear();
+            txtCP.BackColor = Color.White;
+            txtDate.BackColor = Color.White;
+            txtMontant.BackColor = Color.White;
+            txtNom.BackColor = Color.White;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("Fin de l'application", "FIN", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
         }
     }
 }
